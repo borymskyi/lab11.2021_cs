be6754432cd6d282da22878389dc96a2208dd14c
Dmytrii <73241214+bdedwardovich@users.noreply.github.com>
2021-11-22 12:53:32 +0200

    Create lab1.cs

---
+++ lab1.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Lab18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вправа 2.1
+            Console.WriteLine("Введите свое имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Здравствуйте {name}");
+        }
+    }
+}
